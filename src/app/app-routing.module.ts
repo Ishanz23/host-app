d21cf6ed481bb17069fb19c96204d2598177f6b4
Sayantan <web.dev.sayantan@gmail.com>
2023-11-11 22:38:39 +0530

    initial setup for mfe host

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app1',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'remoteAppOne',
+        exposedModule: './Module',
+      }).then((m) => m.AppModule),
+  },
+  {
+    path: 'app2',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'remoteAppTwo',
+        exposedModule: './Module',
+      }).then((m) => m.AppModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
