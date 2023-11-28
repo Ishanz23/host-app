97b547ace5d6020535d37a0640aa131ee36f9425
Sayantan <web.dev.sayantan@gmail.com>
2023-11-29 00:35:19 +0530

    used web component from shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { NgModule } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
+function initialize() {
+  return () => {
+    loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'sharedComponents',
+      exposedModule: './web-components',
+    });
+  };
+}
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initialize,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+declare const require: any;
+const ngVersion = require('../package.json').dependencies['@angular/core'];
+(window as any).plattform = (window as any).plattform || {};
+let platform = (window as any).plattform[ngVersion];
+if (!platform) {
+  platform = platformBrowserDynamic();
+  (window as any).plattform[ngVersion] = platform;
+}
+platform.bootstrapModule(AppModule).catch((err: any) => console.error(err));
